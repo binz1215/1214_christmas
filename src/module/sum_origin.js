0ed83afe59aae37408eb9198bf542fce539a7400
binz1215 <binz1215@naver.com>
2023-12-14 17:17:25 +0900

    주문받은 금액 합산

---
+++ src/module/sum_origin.js
@@ -0,0 +1,51 @@
+import { MENU } from '../constants/contants.js';
+
+class SumOrigin {
+  #order;
+
+  setOrder(orderObjedt) {
+    this.#order = orderObjedt;
+  }
+
+  allPrice() {
+    const menuArray = this.#makeMunuArray();
+    const countArray = this.#makeCountArray();
+    const sum = this.#sum(menuArray, countArray);
+
+    return sum;
+  }
+
+  #sum(menuArray, countArray) {
+    let sum = 0;
+    menuArray.forEach((menuname, index) => {
+      const price = this.#findCategory(menuname);
+      sum += price * countArray[index];
+    });
+    return sum;
+  }
+
+  #findCategory(menu) {
+    for (const category in MENU) {
+      const categoryMenu = MENU[category];
+      if (menu in categoryMenu) {
+        return categoryMenu[menu];
+      }
+    }
+  }
+
+  #makeMunuArray() {
+    const menuArray = this.#order.orders.map((order) => order.menu);
+    return menuArray;
+  }
+
+  #makeCountArray() {
+    const countArray = this.#order.orders.map((count) => count.count);
+    return countArray;
+  }
+}
+export default SumOrigin;
+
+const play = new SumOrigin();
+const x = play.allPrice();
+//
+console.log(x);
