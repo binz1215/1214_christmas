db3295fa624002b031c2d82c7e48b37e121a9399
binz1215 <binz1215@naver.com>
2023-12-14 13:55:11 +0900

    출력사항 작성

--- src/view/outputView.js
+++ src/view/outputView.js
@@ -1,3 +1,10 @@
 import { Console } from '@woowacourse/mission-utils';
 
-Console.print(`${}`);
+export function welcome() {
+  Console.print(`안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.`);
+}
+
+export function preview(day) {
+  Console.print(`12월 ${day}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`);
+  Console.print('');
+}
