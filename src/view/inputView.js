a318254fa9b8e47fc5e9ff71c2201a2df68f73d6
binz1215 <binz1215@naver.com>
2023-12-14 13:51:41 +0900

    입력사항 작성

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,8 +1,16 @@
 import { Console } from '@woowacourse/mission-utils';
 
-async () {
-  const purchase = await Console.readLineAsync('\n');
+export async function inputDay() {
+  const day = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n');
+  return Number(day);
 }
 
+export async function inputPersonNum() {
+  const person = await Console.readLineAsync('방문하실 인원 수를 알려주세요 (1~6사이)\n');
+  return Number(person);
+}
 
-
+export async function inputOrder() {
+  const order = await Console.readLineAsync('주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n');
+  return order;
+}
